0dfe6f6681f131ba2f8e369fb81543cd998de499
des384 <75841716+des384@users.noreply.github.com>
2021-03-01 12:52:12 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@
 document.querySelector(".header__burger").onclick = function (){
     this.classList.toggle("active");
     document.querySelector(".main__menu").classList.toggle("active");
-    document.querySelector("body").classList.toggle("lock");    
+    // document.querySelector("body").classList.toggle("lock");  
+    const scrollY = document.body.style.top;
+document.body.style.position = '';
+document.body.style.top = '';
+window.scrollTo(0, parseInt(scrollY || '0') * -1);  
  }
 
